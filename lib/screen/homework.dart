b062fa6691ef68f084212b0910c21ae1b75bea2f
Dawid Bartniczak <dawid.bartniczak@icloud.com>
2020-07-07 18:59:01 +0200

    Added logic for changing isDone value of homework.

--- lib/screen/homework.dart
+++ lib/screen/homework.dart
@@ -1,86 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
 
-import '../helper/localizationHelper.dart';
 import '../provider/homeworkProvider.dart';
-import '../provider/lessonProvider.dart';
 import '../model/homework.dart';
+import '../widget/testListTile.dart';
 
 class HomeworkScreen extends StatelessWidget {
-  HomeworkProvider _homeworkProvider;
-  LessonProvider _lessonProvider;
-
-  Future<bool> _showDeleteConfirm(BuildContext context) {
-    return showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(LocalizationHelper.of(context).localize('text_confirmation')),
-          content: Text(LocalizationHelper.of(context).localize('homework_remove_message')),
-          actions: <Widget>[
-            FlatButton(
-              child: Text(LocalizationHelper.of(context).localize('text_no')),
-              onPressed: () => Navigator.of(context).pop(false),
-            ),
-            FlatButton(
-              child: Text(LocalizationHelper.of(context).localize('text_yes')),
-              textColor: Colors.red,
-              onPressed: () => Navigator.of(context).pop(true),
-            )
-          ],
-        );
-      }
-    );
-  }
-
-  Widget _buildTistTile(Homework homework, BuildContext context) {
-    return Dismissible(
-      key: ValueKey(homework.id),
-      background: Container(
-        alignment: Alignment.centerRight,
-        padding: EdgeInsets.all(16),
-        color: Theme.of(context).errorColor,
-        child: Icon(Icons.delete),
-      ),
-      direction: DismissDirection.endToStart,
-      confirmDismiss: (_) => _showDeleteConfirm(context),
-      onDismissed: (_) => _homeworkProvider.deleteHomework(homework.id),
-      child: ListTile(
-        title: Text(homework.name),
-        subtitle: Text(_lessonProvider.lessonSubjectForId(homework.lessonID)),
-        leading: CircleAvatar(
-          radius: 24,
-          child: Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: FittedBox(
-              fit: BoxFit.scaleDown,
-              child: Text(DateFormat('dd.MM').format(homework.date)),
-            ),
-          ),
-        ),
-        trailing: IconButton(
-          icon: const Icon(Icons.check),
-          color: !homework.isDone ? Colors.grey : Colors.green,
-          onPressed: () {
-            
-          },
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    _homeworkProvider = Provider.of<HomeworkProvider>(context);
-    _lessonProvider = Provider.of<LessonProvider>(context);
-    List<Homework> homework = _homeworkProvider.homework;
+    HomeworkProvider homeworkProvider = Provider.of<HomeworkProvider>(context);
+    List<Homework> homework = homeworkProvider.homework;
 
     return homework.length > 0
       ? ListView(
         children: 
-          homework.map((Homework homework) => _buildTistTile(homework, context)).toList(),
+          homework.map((Homework homework) => 
+            ChangeNotifierProvider.value(
+              value: homework,
+              child: TestListTile(),
+            )
+          ).toList(),
       )
       : Center(
         child: Text('Brak zadań domowych.'),
--- lib/widget/testListTile.dart
+++ lib/widget/testListTile.dart
@@ -4,12 +4,10 @@ import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 
 import '../helper/localizationHelper.dart';
-import '../provider/testProvider.dart';
-import '../model/test.dart';
+import '../provider/homeworkProvider.dart';
+import '../model/homework.dart';
 
-class TestScreen extends StatelessWidget {
-  TestProvider _testProvider;
-  LessonProvider _lessonProvider;
+class TestListTile extends StatelessWidget {
 
   Future<bool> _showDeleteConfirm(BuildContext context) {
     return showDialog(
@@ -18,7 +16,7 @@ class TestScreen extends StatelessWidget {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text(LocalizationHelper.of(context).localize('text_confirmation')),
-          content: Text(LocalizationHelper.of(context).localize('test_remove_message')),
+          content: Text(LocalizationHelper.of(context).localize('homework_remove_message')),
           actions: <Widget>[
             FlatButton(
               child: Text(LocalizationHelper.of(context).localize('text_no')),
@@ -27,7 +25,7 @@ class TestScreen extends StatelessWidget {
             FlatButton(
               child: Text(LocalizationHelper.of(context).localize('text_yes')),
               textColor: Colors.red,
-              onPressed: () => Navigator.of(context).pop(true), 
+              onPressed: () => Navigator.of(context).pop(true),
             )
           ],
         );
@@ -35,9 +33,14 @@ class TestScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildTestTile(Test test, BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
+    Homework homework = Provider.of<Homework>(context, listen: false);
+    HomeworkProvider _homeworkProvider = Provider.of<HomeworkProvider>(context, listen: false);
+    LessonProvider _lessonProvider = Provider.of<LessonProvider>(context, listen: false);
+
     return Dismissible(
-      key: ValueKey(test.id),
+      key: ValueKey(homework.id),
       background: Container(
         alignment: Alignment.centerRight,
         padding: EdgeInsets.all(16),
@@ -46,36 +49,30 @@ class TestScreen extends StatelessWidget {
       ),
       direction: DismissDirection.endToStart,
       confirmDismiss: (_) => _showDeleteConfirm(context),
-      onDismissed: (_) => _testProvider.deleteTest(test.id),
+      onDismissed: (_) => _homeworkProvider.deleteHomework(homework.id),
       child: ListTile(
-        title: Text(test.name),
-        subtitle: Text(_lessonProvider.lessonSubjectForId(test.lessonID)),
+        title: Text(homework.name),
+        subtitle: Text(_lessonProvider.lessonSubjectForId(homework.lessonID)),
         leading: CircleAvatar(
           radius: 24,
-          child: FittedBox(
-            fit: BoxFit.scaleDown,
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: Text(DateFormat('dd.MM').format(test.date)),
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: FittedBox(
+              fit: BoxFit.scaleDown,
+              child: Text(DateFormat('dd.MM').format(homework.date)),
             ),
           ),
         ),
+        trailing: Consumer<Homework>(
+          builder: (BuildContext context, Homework homework, _) {
+            return IconButton(
+              icon: const Icon(Icons.check),
+              color: !homework.isDone ? Colors.grey : Colors.green,
+              onPressed: homework.changeIsDone,
+            );
+          },
+        ),
       ),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    _testProvider = Provider.of<TestProvider>(context);
-    _lessonProvider = Provider.of<LessonProvider>(context);
-    List<Test> tests = _testProvider.tests;
-
-    return tests.length > 0
-      ? ListView(
-        children: tests.map((Test test) => _buildTestTile(test, context)).toList(),
-      )
-      : Center(
-        child: Text('Brak sprawdzianów.'),
-      );
   }
 }
